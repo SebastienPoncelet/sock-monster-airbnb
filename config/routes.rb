801a8559a83b908a1b35c3272fe0c74be42722e9
Sebastien Poncelet <sebastienponcelet@hotmail.com>
2018-11-20 18:52:46 +0800

    Bookings controller generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'machines/index'
-  get 'machines/show'
-  get 'machines/new'
-  get 'machines/create'
-  get 'machines/edit'
-  get 'machines/update'
-  get 'machines/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #route for Wechat Open_Id verification
