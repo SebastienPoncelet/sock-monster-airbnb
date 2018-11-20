8173895243a597536f061592b6cd962888fb58cb
Sebastien Poncelet <sebastienponcelet@hotmail.com>
2018-11-20 13:31:56 +0800

    Added validation and a belong to through machines to Booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,12 @@
 class Booking < ApplicationRecord
+  # User who made a booking
   belongs_to :users
+  # link to machine owner through the machines table
+  belongs_to :users, through: :machines
   belongs_to :machines
   has_many :reviews, dependent: :destroy
+
+  validates :dates, presence: true
+  validates :price, presence: true
+  validates :status, presence: true
 end
