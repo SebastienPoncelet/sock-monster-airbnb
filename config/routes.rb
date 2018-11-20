adf7cccf1afba7efedc65ebe664ea4d6ee6bdd74
Florent Ayot <ayotflorent@gmail.com>
2018-11-20 18:09:49 +0800

    added the users API Show

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :machines, only: [ :index ]
+      resources :users, only: [ :index, :show ]
     end
   end
 
