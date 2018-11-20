7f04292715fc7c93a3b5047155c41b98249e5434
Sebastien Poncelet <sebastienponcelet@hotmail.com>
2018-11-20 12:01:20 +0800

    Updated validations on Machine model

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -3,9 +3,7 @@ class Machine < ApplicationRecord
   has_many :bookings, dependent: :destroy
 
   validates :name, presence: true
-  validates :brand, presence: true
   validates :sock_count, presence: true
-  validates :type, presence: true
   validates :location, presence: true
   validates :availability, presence: true
   validates :price, presence: true
